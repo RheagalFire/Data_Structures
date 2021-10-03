59ce7ea3c5fba28c61a2f9cd91de084fa4d47eb0
Aarish Alam <arishalam121@gmail.com>
2021-10-03 11:28:17 +0530

    Create count_n_triangles.cpp
    Count the number of triangles in O(n^2) time complexity

---
+++ Arrays/count_n_triangles.cpp
@@ -0,0 +1,25 @@
+int findNumberOfTriangles(int arr[], int n)
+    {
+        sort(arr,arr+n);
+        int count=0;
+        for(int i=n-1;i>=0;i--)
+        {
+            int l=0;
+            int r=i-1;
+            while(l<r)
+            {
+                if(arr[l]+arr[r]>arr[i])
+                {
+                    count+=(r-l);
+                    r--;
+                }
+                else
+                {
+                    l++;
+                }
+            }
+        }
+        
+        return count;
+        
+    }
